de48896dc904ce5b7519c591393d9090562b82ab
Sonu Kachhap <166591588+Sonu-77@users.noreply.github.com>
2024-09-03 16:27:11 +0000

    Setup Project

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+import Home from "./components/Home";
+
+
+function App() {
+
+
+  return (
+    <>
+
+        <div className=" text-[#087897] bg-amber-500 text-[9vw]  ">
+          <h1>Sonu</h1>
+          <h1>Kachhap</h1>
+        </div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Home></Home>
+        
+    </>
+  );
+}
+
+export default App;
---
+++ Portfolio/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='flex bg-amber-500 text-[9vw]'  >Home</div>
+  )
+}
+
+export default Home
