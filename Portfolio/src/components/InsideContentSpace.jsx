d2d9decee6d84223ba30abf2f238d29af58f96f6
Sonu-77 <sonukachhap77@gmail.com>
2024-09-11 20:53:40 +0530

    about page done

--- Portfolio/src/components/InsideContentSpace.jsx
+++ Portfolio/src/components/InsideContentSpace.jsx
@@ -1,67 +1,77 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import { FaChevronLeft } from "react-icons/fa";
 import { FaChevronRight } from "react-icons/fa";
 import { VscChromeClose } from "react-icons/vsc";
-import About from './contentfiles/About';
+import About from "./contentfiles/About";
 
 function InsideContentSpace({ data }) {
-    return (
-        <div className='relative '>
-            <div className='lg:h-[47.5vw] sm:z-10 lg:w-[25vw] sm:h-[40vw] sm:w-[94vw] fixed sm:ml-[3vw] lg:right-[1vw] lg:top-[1vw] rounded-xl sm:flex lg:flex lg:flex-col lg:items-center overflow-hidden '
-                style={{ background: `linear-gradient(to left,${data.from},${data.to})` }}
-            >
+  return (
+    <div className="relative ">
+      <div
+        className="lg:h-[47.5vw] sm:z-10 lg:w-[25vw] sm:h-[40vw] sm:w-[94vw] fixed sm:ml-[3vw] lg:right-[1vw] lg:top-[1vw] rounded-xl sm:flex lg:flex lg:flex-col lg:items-center overflow-hidden "
+        style={{
+          background: `linear-gradient(to left,${data.from},${data.to})`,
+        }}
+      >
+        <div
+          className={`lg:w-[14vw] sm:w-[28vw] lg:h-[14vw] sm:h-[28vw]  z-10 absolute lg:-right-[4vw] sm:-right-[3vw] sm:-top-[3vw] lg:-top-[1.3vw] rounded-full`}
+          style={{
+            background: `linear-gradient(to left,${data.from},${data.to})`,
+          }}
+        ></div>
+        <div
+          className={`lg:w-[16vw] lg:h-[16vw] opacity-[0.4] sm:h-[35vw] sm:w-[35vw]  transition-all duration-300 absolute lg:-right-[4vw] lg:-top-[1.8vw] sm:-right-[3vw] sm:-top-[3vw] rounded-full `}
+          style={{
+            background: `linear-gradient(to top,${data.from},${data.to})`,
+          }}
+        ></div>
 
-                <div className={`lg:w-[14vw] sm:w-[28vw] lg:h-[14vw] sm:h-[28vw]  z-10 absolute lg:-right-[4vw] sm:-right-[3vw] sm:-top-[3vw] lg:-top-[1.3vw] rounded-full`}
-                    style={{ background: `linear-gradient(to left,${data.from},${data.to})` }}>
-
-                </div>
-                <div className={`lg:w-[16vw] lg:h-[16vw] opacity-[0.4] sm:h-[35vw] sm:w-[35vw]  transition-all duration-300 absolute lg:-right-[4vw] lg:-top-[1.8vw] sm:-right-[3vw] sm:-top-[3vw] rounded-full `}
-                    style={{ background: `linear-gradient(to top,${data.from},${data.to})` }}>
-                </div>
-
-                <div className='lg:h-[40vw] sm:h-[35vw] sm:w-full  lg:w-[22vw] group lg:mt-[2vw] sm:mt-[3vw] lg:border-b-[0.06vw]  lg:border-[#cfd9df] flex flex-col lg:items-end lg:justify-between '>
-                        <div className='lg:text-[1.6vw] sm:text-[5vw] z-20  lg:h-[3vw] lg:w-[3vw] flex justify-center items-center lg:group-hover:text-[2.4vw] text-[#cfd9df] transition-all duration-300 ease-in-out sm:absolute lg:relative sm:right-[4vw] lg:pl-[5vw] ' >
-                    <Link to={"/"}>
-                            <VscChromeClose />
-                    </Link>
-                        </div>
-                    <div className='lg:h-[26vw] lg:w-[10vw]  lg:mb-[1vw]  lg:flex lg:flex-col lg:justify-between lg:items-center '>
-                        <div className='lg:h-[15vw] lg:w-[10vw]  lg:pt-[2vw]  '>
-                            <h2 className='lg:rotate-[270deg]  sm:absolute lg:h-[5vw] lg:w-[12vw] lg:pl-1 lg:pt-3  lg:tracking-widest lg:mt-[4vw] sm:mt-[10vw] sm:ml-[30vw] lg:ml-0 uppercase text-[#ffff]  roboto-medium lg:text-[2vw] sm:text-[5vw] '>
-                                {data.title}
-                            </h2>
-                        </div>
-                        <div className='lg:w-[9vw] lg:h-[9vw] sm:w-[27vw] sm:h-[27vw] flex justify-center items-center rounded-full'>
-                            <img className='lg:w-[6vw] lg:h-[6vw] sm:w-[23vw] sm:h-[23vw] rounded-full object-cover object-center' src={data.image} alt="pic" />
-                        </div>
-                    </div>
-                </div>
-                <div className='lg:flex lg:h-[5vw] lg:w-[22vw] sm:hidden  lg:justify-between text-[#fff]'>
-                    <div className='lg:flex lg:justify-center lg:items-center lg:gap-[1vw]'>
-                        <FaChevronLeft />
-                        <Link to={`/${data.left}`}><h3 className='uppercase lg:text-[1vw] lg:tracking-wide  '>{data.left}</h3></Link>
-                    </div>
-                    <div className='lg:flex lg:justify-center  lg:items-center lg:gap-[1vw]'>
-                    <Link to={`/${data.right}`}><h3 className='uppercase lg:text-[1vw] lg:tracking-wide  '>{data.right}</h3></Link>
-                        <FaChevronRight />
-                    </div>
-                </div>
+        <div className="lg:h-[40vw] sm:h-[35vw] sm:w-full  lg:w-[22vw] group lg:mt-[2vw] sm:mt-[3vw] lg:border-b-[0.06vw]  lg:border-[#cfd9df] flex flex-col lg:items-end lg:justify-between ">
+          <div className="lg:text-[1.6vw] sm:text-[5vw] z-20  lg:h-[3vw] lg:w-[3vw] flex justify-center items-center lg:group-hover:text-[2.4vw] text-[#cfd9df] transition-all duration-300 ease-in-out sm:absolute lg:relative sm:right-[4vw] lg:pl-[5vw] ">
+            <Link to={"/"}>
+              <VscChromeClose />
+            </Link>
+          </div>
+          <div className="lg:h-[26vw] lg:w-[10vw]  lg:mb-[1vw]  lg:flex lg:flex-col lg:justify-between lg:items-center ">
+            <div className="lg:h-[15vw] lg:w-[10vw]  lg:pt-[2vw]  ">
+              <h2 className="lg:rotate-[270deg]  sm:absolute lg:h-[5vw] lg:w-[12vw] lg:pl-1 lg:pt-3  lg:tracking-widest lg:mt-[4vw] sm:mt-[10vw] sm:ml-[30vw] lg:ml-0 uppercase text-[#ffff]  roboto-medium lg:text-[2vw] sm:text-[5vw] ">
+                {data.title}
+              </h2>
             </div>
-            <div className='lg:w-[100vw] lg:h-[100vw] sm:w-[94vw] sm:h-[200vw] sm:absolute sm:ml-[3vw] lg:relative lg:ml-0 lg:mt-0  sm:mt-[41vw]  lg:flex lg:justify-center  '
-            style={{ background: `linear-gradient(to bottom right,#0d0f0e,#b21f1f,#000000,#43C6AC,#1a2a6c,#0e0839,#450d39,#F27121,#cbb4d4,#000000)` }}>
-                <div className='lg:w-[43vw] absolute left-[16vw] lg:h-[80vw]  '>
-
-                    <About>
-
-                    </About>
-                </div>
-
+            <div className="lg:w-[9vw] lg:h-[9vw] sm:w-[27vw] sm:h-[27vw] flex justify-center items-center rounded-full">
+              <img
+                className="lg:w-[6vw] lg:h-[6vw] sm:w-[23vw] sm:h-[23vw] rounded-full object-cover object-center"
+                src={data.image}
+                alt="pic"
+              />
             </div>
-
-
+          </div>
         </div>
-    )
+        <div className="lg:flex lg:h-[5vw] lg:w-[22vw] sm:hidden  lg:justify-between text-[#fff]">
+          <div className="lg:flex lg:justify-center lg:items-center lg:gap-[1vw]">
+            <FaChevronLeft />
+            <Link to={`/${data.left}`}>
+              <h3 className="uppercase lg:text-[1vw] lg:tracking-wide  ">
+                {data.left}
+              </h3>
+            </Link>
+          </div>
+          <div className="lg:flex lg:justify-center  lg:items-center lg:gap-[1vw]">
+            <Link to={`/${data.right}`}>
+              <h3 className="uppercase lg:text-[1vw] lg:tracking-wide  ">
+                {data.right}
+              </h3>
+            </Link>
+            <FaChevronRight />
+          </div>
+        </div>
+      </div>
+      <div className="lg:w-[74vw] lg:h-[100vw] sm:w-[94vw] sm:h-[200vw]  sm:absolute sm:ml-[3vw] lg:relative lg:ml-0 lg:mt-0  sm:mt-[41vw]  lg:flex lg:justify-center  ">
+        <About></About>
+      </div>
+    </div>
+  );
 }
 
-export default InsideContentSpace
+export default InsideContentSpace;
--- Portfolio/src/components/contentfiles/About.jsx
+++ Portfolio/src/components/contentfiles/About.jsx
@@ -1,56 +1,111 @@
-import React from 'react'
+import React from "react";
 
 function About() {
-    return (
-        <>
-        <div className='flex flex-col gap-[3vw] lato-regular text-[#ffff]'>
-            <div className='flex '>
-
-                <div className='lg:w-[30vw] lg:h-[15vw] lg:p-[1vw] rounded-lg bg-[#0b4892] lg:mt-[10vw] '>
-
-                    <p className='text-[1.4vw]' >
-                    Hi, I'm Sonu Kachhap, a frontend developer from Jharkhand, currently pursuing my studies at University of Mysore. I have a deep passion for web development and a strong desire to create visually appealing and highly functional websites.
-                    </p>
+  return (
+    <>
+      <div className="lg:w-[74vw] lg:h-[100vw] relative">
+        <div className="lg:w-[43vw] absolute left-[16vw] lg:h-[80vw]  ">
+          <div className="lg:mt-[7vw] lg:w-[15vw] lg:h-[5vw]  ">
+            <h2 className="text-[2.5vw] font-bold tracking-wider uppercase lg:ml-[1vw]"
+            style={{
+                backgroundImage: 'linear-gradient(to  right, #23074d,#cc5333)',
+                WebkitBackgroundClip: 'text', // For Safari and Chrome
+                backgroundClip: 'text', // For other browsers
+                WebkitTextFillColor: 'transparent', // Makes the text transparent in Webkit-based browsers
+              }}
+              >About Me</h2>
+          </div>
+          <div className="flex flex-col gap-[3vw] lg:mt-[2vw]">
+            <div
+              className="lg:h-[30.2vw] lg:w-[25vw] ml-[1vw]  rounded-tr-[4vw] relative"
+              style={{
+                background: `linear-gradient(to bottom right,#bdc3c7,#2c3e50,#c31432,#240b36,#f5af19)`,
+              }}
+            >
+              <div className="absolute z-10 rotate-[270deg] top-[14vw] -right-[9vw]">
+                <h2 className="text-[3vw] tracking-wide uppercase dancing-script-font "
+                style={{
+                    backgroundImage: 'linear-gradient(to  right, #2E3192,#1BFFFF)',
+                    WebkitBackgroundClip: 'text', // For Safari and Chrome
+                    backgroundClip: 'text', // For other browsers
+                    WebkitTextFillColor: 'transparent', // Makes the text transparent in Webkit-based browsers
+                  }}>
+                  Sonu Kachhap
+                </h2>
+              </div>
+              <img
+                className="lg:h-[30vw] lg:w-[25vw] object-cover rounded-tr-[4vw] absolute -left-1  "
+                src="https://images.pexels.com/photos/1813923/pexels-photo-1813923.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt="pic"
+              />
+            </div>
+            <div className="group flex flex-col gap-[1.5vw]">
+              <h2 className="lg:ml-[30vw] lg:h-[5vw] lg:w-[10vw] flex justify-center items-center  dancing-script-font group-hover:text-[2.5vw] uppercase text-[2vw] transition-all duration-300 ease-in-out "
+              style={{
+                backgroundImage: 'linear-gradient(to  right, #2E3192,#1BFFFF)',
+                WebkitBackgroundClip: 'text', // For Safari and Chrome
+                backgroundClip: 'text', // For other browsers
+                WebkitTextFillColor: 'transparent', // Makes the text transparent in Webkit-based browsers
+              }}>
+                Intro
+              </h2>
+              <div
+                className="lg:h-[20vw] lg:w-[30vw] ml-[1vw] bg-[#000000] rounded-xl lg:ml-[20vw]  relative"
+                style={{
+                  background: `linear-gradient(to bottom right,#bdc3c7,#2c3e50,#c31432,#240b36,#f5af19)`,
+                }}
+              >
+                <div
+                  className="lg:h-[20vw] lg:w-[30vw]  absolute  rounded-xl -left-1  -top-1 p-[1vw] "
+                  style={{
+                    background: `linear-gradient(to bottom right,#2E3192,#1BFFFF)`,
+                  }}
+                >
+                  <p className="text-[1.2vw] text-[#ffff] ">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Consectetur incidunt porro ipsam facilis provident dolor
+                    explicabo modi sit earum. Impedit atque magni magnam
+                    numquam, mollitia eligendi ipsa neque cumque sonuautem.
+                  </p>
                 </div>
-                <div className='lg:mt-[10vw]'><h2> career Intro</h2></div>
+              </div>
             </div>
-
-            <div className=' relative lg:w-[55vw] lg:h-[21vw] items-center'>
-
-
-                <div className='lg:w-[30vw] lg:h-[21vw] lg:p-[1vw] rounded-lg bg-[#0b4892] absolute right-0 '>
-
-                    <p className='text-[1.4vw]'>
-                    My journey in frontend development began with a fascination for how design and code come together to create user-friendly experiences. I enjoy the challenge of translating complex ideas into simple, intuitive interfaces. With a solid foundation in HTML, CSS, and JavaScript, I also explore modern tools and frameworks like React.js to stay at the forefront of web development.
-                    </p>
+            <div className="group flex flex-col gap-[1.5vw]">
+            <h2 className=" lg:h-[5vw] lg:w-[13vw] flex justify-center items-center transition-all duration-300 ease-in-out dancing-script-font group-hover:text-[2.5vw] uppercase text-[2vw] "
+            style={{
+                backgroundImage: 'linear-gradient(to  right, #2E3192,#1BFFFF)',
+                WebkitBackgroundClip: 'text', // For Safari and Chrome
+                backgroundClip: 'text', // For other browsers
+                WebkitTextFillColor: 'transparent', // Makes the text transparent in Webkit-based browsers
+              }}>
+                hobbies
+              </h2>
+              <div
+                className="lg:h-[20vw] lg:w-[30vw] ml-[1vw] bg-[#000000] rounded-xl   relative"
+                style={{
+                  background: `linear-gradient(to bottom right,#bdc3c7,#2c3e50,#c31432,#240b36,#f5af19)`,
+                }}
+              >
+                <div
+                  className="lg:h-[20vw] lg:w-[30vw] absolute  rounded-xl -left-1  -top-1 p-[1vw] "
+                  style={{
+                    background: `linear-gradient(to bottom right,#2E3192,#1BFFFF)`,
+                  }}
+                >
+                  <p className="text-[1.2vw] text-[#ffff] ">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Consectetur incidunt porro ipsam facilis provident dolor
+                    explicabo modi sit earum. Impedit atque magni magnam
+                    numquam, mollitia eligendi ipsa neque cumque sonuautem.
+                  </p>
                 </div>
+              </div>
             </div>
-
-            <div className='flex '>
-
-                <div className='lg:w-[30vw] lg:h-[12vw] lg:p-[1vw] rounded-lg bg-[#0b4892]  '>
-
-                    <p className='text-[1.4vw]' >
-                    Traveling: Exploring new places and cultures, always seeking new adventures and experiences.
-                    </p>
-                </div>
-                <div className=''><h2> Hobbies</h2></div>
-            </div>
-
-            
-            <div className='lg:w-[30vw] lg:h-[13vw] lg:p-[1vw] rounded-lg bg-[#0b4892] lg:ml-[20vw] '>
-
-                <p className='text-[1.4vw]'>
-                Sports: Playing and enjoying sports like football, volleyball, basketball, and badminton.
-
-                </p>
-            </div>
+          </div>
         </div>
-        </>
-
-
-
-    )
+      </div>
+    </>
+  );
 }
 
-export default About
+export default About;
